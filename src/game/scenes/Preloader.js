2e07400872fb12ac77f266cb800baac303062ab6
sb.kim <dinoksb@gmail.com>
2025-01-13 17:47:30 +0900

    initial commit

---
+++ src/game/scenes/Preloader.js
@@ -0,0 +1,28 @@
+import { Scene } from "phaser";
+
+export class Preloader extends Scene {
+    constructor() {
+        super("Preloader");
+    }
+
+    preload() {
+        this.load.setPath("assets");
+        // this.load.image([
+        //     'bg',
+        //     'logo',
+        // ]);
+
+        this.load.image('background', 'bg.png');
+        this.load.image('logo', 'logo.png');
+
+        this.load.tilemapTiledJSON('level_1', 'mapDatas/level_1.json');
+        this.load.image('tile', 'textures/tiles/default_tile.png');
+
+        this.load.image('box', 'textures/character/box.png');
+
+        // when load completed
+        this.load.on('complete', () =>{
+            this.scene.start("MainMenu");
+        });
+    }
+}
