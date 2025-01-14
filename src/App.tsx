97633f5da5abce5e3fb0f3d156fe728ecb8fd9da
sb.kim <seungbae@planetariumhq.com>
2025-01-14 18:11:30 +0900

    refactor: Migrate project to TypeScript and update dependencies
    - Renamed project from 'template-react' to 'template-react-ts' to reflect TypeScript integration.
    - Updated package versions for React and related dependencies to improve compatibility.
    - Removed obsolete App and main components, streamlining the project structure.
    - Added TypeScript ESLint plugins for enhanced linting support.
    - Introduced TypeScript as a development dependency for better type safety.

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
+import { MainMenu } from "./game/scenes/MainMenu";
+
+function App() {
+    //  References to the PhaserGame component (game and scene are exposed)
+    const phaserRef = useRef<IRefPhaserGame | null>(null);
+
+    const changeScene = () => {
+        if (phaserRef.current) {
+            const scene = phaserRef.current.scene as MainMenu;
+
+            if (scene) {
+                scene.changeScene();
+            }
+        }
+    };
+
+    return (
+        <div id="app">
+            <PhaserGame ref={phaserRef} />
+            <div>
+                <button className="button" onClick={changeScene}>
+                    Change Scene
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
