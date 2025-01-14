b98332c166d5b245c195d975ade241ae0ac51e02
sb.kim <seungbae@planetariumhq.com>
2025-01-14 10:17:52 +0900

    chore: Update dependencies in package-lock.json
    - Bump versions of multiple Rollup plugins and related packages to 4.30.1.
    - Update @types/estree from 1.0.5 to 1.0.6.
    - Upgrade cross-spawn to version 7.0.6.
    - Update postcss to version 8.5.0 and its dependencies.
    - Add new FreeBSD and Linux Loongarch64 Rollup plugins.
    - Ensure all updated packages include license information.

--- src/game/core/Player.js
+++ src/game/core/Player.js
@@ -1,22 +1,19 @@
 export class Player extends Phaser.GameObjects.Sprite {
     constructor(config) {
-        super(config.scene, config.x, config.y, 'box');
+        super(config.scene, config.x, config.y, "box");
 
         this.scene = config.scene;
         this.scene.add.existing(this);
         this.scene.physics.world.enable(this);
 
-        this.playerSpeed = 200
-        this.playerJump = 400
+        this.playerSpeed = 200;
+        this.playerJump = 400;
 
         this.body.setGravityY(config.settings.gravity.y);
         this.body.velocity.x = this.playerSpeed;
 
         this.canJump = true;
         this.onWall = false;
-
-        console.log(this.body.width);
-        
 
         this.scene.input.on("pointerdown", this.handleJump, this);
     }
@@ -27,17 +24,16 @@ export class Player extends Phaser.GameObjects.Sprite {
 
             if (this.onWall) {
                 this.scaleX *= -1;
-                this.body.velocity.x =
-                this.playerSpeed * this.scaleX;
+                this.body.velocity.x = this.playerSpeed * this.scaleX;
             }
 
             this.canJump = false;
             this.onWall = false;
 
             if (this.scaleX === -1) {
-                this.body.setOffset(this.body.width, 0)
+                this.body.setOffset(this.body.width, 0);
             } else {
-                this.body.setOffset(0, 0)
+                this.body.setOffset(0, 0);
             }
         }
     }
@@ -68,9 +64,9 @@ export class Player extends Phaser.GameObjects.Sprite {
         }
 
         if (this.scaleX === -1) {
-            this.body.setOffset(this.body.width, 0)
+            this.body.setOffset(this.body.width, 0);
         } else {
-            this.body.setOffset(0, 0)
+            this.body.setOffset(0, 0);
         }
 
         this.body.velocity.x = this.playerSpeed * this.scaleX;
