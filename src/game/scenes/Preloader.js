fc8c80016c5c3bdd776dc1b70e7785c0da6a915e
sb.kim <seungbae@planetariumhq.com>
2025-01-14 17:23:44 +0900

    refactor: Standardize image loading syntax in Preloader.js
    - Updated image loading syntax to use consistent double quotes.
    - Renamed tilemap loading key from 'level01' to 'tilelayer' for clarity.
    - Ensured proper formatting and consistency in the preload method.

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -8,16 +8,16 @@ export class Preloader extends Scene {
     preload() {
         this.load.setPath("assets");
 
-        this.load.image('background', 'bg.png');
-        this.load.image('logo', 'logo.png');
+        this.load.image("background", "bg.png");
+        this.load.image("logo", "logo.png");
 
-        this.load.tilemapTiledJSON('level01', 'mapDatas/level01.json');
-        this.load.image('tile', 'textures/tiles/default_tile.png');
+        this.load.tilemapTiledJSON("tilelayer", "mapDatas/level01.json");
+        this.load.image("tile", "textures/tiles/default_tile.png");
 
-        this.load.image('box', 'textures/character/box.png');
+        this.load.image("box", "textures/character/box.png");
 
         // when load completed
-        this.load.on('complete', () =>{
+        this.load.on("complete", () => {
             this.scene.start("MainMenu");
         });
     }
